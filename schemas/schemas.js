8f8b5d658e8565f3d8c3311926f3d5aab70c2af2
ezezbogar <ezezbogar@gmail.com>
2022-04-27 15:26:24 -0300

    connect song methods to music service

--- schemas/schemas.js
+++ schemas/schemas.js
@@ -4,11 +4,18 @@ const songSchema = S.object()
 	.id("songSchema")
 	.title("Song")
 	.description("Schema used for songs")
-	.prop("id", S.number().required())
 	.prop("uri", S.string().required())
 	.prop("title", S.string().required())
 	.prop("artist_id", S.number().required())
 	.prop("album_id", S.number());
+
+const songEditSchema = S.object()
+	.id("songEditSchema")
+	.title("SongEdit")
+	.description("Schema used for song edition")
+	.prop("title", S.string())
+	.prop("album_id", S.number())
+	.prop("blocked", S.boolean())
 
 const albumSchema = S.object()
 	.id("albumSchema")
@@ -29,13 +36,14 @@ const playlistSchema = S.object()
 	.prop("songs", S.array(songSchema));
 
 // Para cargarlos rapido en swagger
-const schemas = [songSchema, albumSchema, playlistSchema];
+const schemas = [songSchema, albumSchema, playlistSchema, songEditSchema];
 module.exports.schemas = schemas;
 
 // Los agrego por separado para que aparezcan en los request body en swagger
 module.exports.songSchema = songSchema;
 module.exports.albumSchema = albumSchema;
 module.exports.playlistSchema = playlistSchema;
+module.exports.songEditSchema = songEditSchema;
 
 
 
