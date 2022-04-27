26e3ad975f3b652371fc912b1422975f3d213b0a
ezezbogar <ezezbogar@gmail.com>
2022-04-27 19:36:01 -0300

    connect playlist methods to music service

--- schemas/schemas.js
+++ schemas/schemas.js
@@ -34,13 +34,18 @@ const playlistSchema = S.object()
 	.id("playlistSchema")
 	.title("Playlist")
 	.description("Schema used for playlists")
-	.prop("id", S.number().required())
 	.prop("title", S.string().required())
-	.prop("owner_id", S.number().required())
-	.prop("songs", S.array(songSchema));
+	.prop("owner_id", S.number().required());
+
+const playlistEditSchema = S.object()
+	.id("playlistEditSchema")
+	.title("PlaylistEdit")
+	.description("Schema used for playlist edition")
+	.prop("title", S.string());
 
 // Para cargarlos rapido en swagger
-const schemas = [songSchema, albumSchema, playlistSchema, songEditSchema, albumEditSchema];
+const schemas = [songSchema, albumSchema, playlistSchema,
+	songEditSchema, albumEditSchema, playlistEditSchema];
 module.exports.schemas = schemas;
 
 // Los agrego por separado para que aparezcan en los request body en swagger
@@ -49,6 +54,7 @@ module.exports.albumSchema = albumSchema;
 module.exports.playlistSchema = playlistSchema;
 module.exports.songEditSchema = songEditSchema;
 module.exports.albumEditSchema = albumEditSchema;
+module.exports.playlistEditSchema = playlistEditSchema;
 
 
 
