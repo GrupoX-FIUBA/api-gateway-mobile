626d5bb7e962f459dc218284c5ac6911a5e97581
ezezbogar <ezezbogar@gmail.com>
2022-05-30 13:12:35 -0300

    fix linter

--- controllers/general.js
+++ controllers/general.js
@@ -1,4 +1,4 @@
 exports.headerCORS =  async (req, reply) => {
 	reply.code(200).header("Access-Control-Allow-Methods", "*")
-	.header("Access-Control-Allow-Origin", "*").send();
-}
+		.header("Access-Control-Allow-Origin", "*").send();
+};
