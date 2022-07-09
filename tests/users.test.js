f67ab6e6b477e212a56ed08feaabdeba18f13613
ezezbogar <ezezbogar@gmail.com>
2022-07-09 20:28:01 -0300

    refactor tests with jest, mock axios

---
+++ tests/users.test.js
@@ -0,0 +1,59 @@
+"use strict";
+
+const build = require("../app");
+
+const user = {};
+
+user.data = {
+	"name": "Marolio",
+	"subscription": "Regular",
+	"federated": false,
+	"admin": false,
+	"disabled": false,
+	"photo_url": "https://firebasestorage.googleapis.com/some-users",
+	"following": [],
+	"followers": [],
+	"genres": [],
+	"uid": "RAPzpynEsnYkGi7K3y9lDRy0b0b2",
+	"email": "prueba@gmail.com"
+};
+
+jest.mock("axios", () => {
+	return {
+		create: jest.fn(() => ({
+			get: jest.fn(() => user),
+			interceptors: {
+				request: { use: jest.fn(), eject: jest.fn() },
+				response: { use: jest.fn(), eject: jest.fn() }
+			}
+		}))
+	};
+});
+
+jest.mock("../fire/fire", () => {
+	return {
+		Fire: jest.fn(() => ({
+			objectExists: jest.fn(() => Promise.resolve(true)),
+			getResourceURI: jest.fn(() => Promise.resolve("resource-uri.com")),
+		}))
+	};
+});
+
+describe("when users API calls are successful", () => {
+	it("should return the corresponding user", async () => {
+		const app = build();
+		
+		var expectedUser = JSON.parse(JSON.stringify(user.data));
+		expectedUser.image = "resource-uri.com";
+
+		const result = await app.inject({
+			method: "GET",
+			url: "/users/:user_id",
+			params: {
+				user_id: "RAPzpynEsnYkGi7K3y9lDRy0b0b2",
+			},
+		});
+
+		expect(result.payload).toBe(JSON.stringify(expectedUser));
+	});
+});
